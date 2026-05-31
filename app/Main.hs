ec4bddedc2653b09e66180eacdb1e7e59ffbd9e5
gKrokod <t9013054362@gmail.com>
2026-05-31 18:18:53 +0300

    add current directory, no path

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import Control.Concurrent.STM
 import Control.Concurrent.Async 
 import Hotkey.Types
+import System.Directory (getCurrentDirectory,)
 
 import qualified Handlers.Engine
 import Handlers.Logger (Log (..))
@@ -11,19 +12,24 @@ import qualified Logger
 import qualified Engine
 import Hotkey.Grab (getKey)
 
-
 main :: IO ()
 main = do
   pause <- atomically $ newTVar Off
   offset <- atomically $ newTVar 0 
-  -- dir <- getCurrentDirectory - for realise 
+  dir <- getCurrentDirectory
+  let
 #ifdef mingw32_HOST_OS
-  let dir ="C:\\sharedFolder\\test" -- windows
       file = dir <> "\\jukebox.json"
 #else
-  let dir ="/home/m/share/sharedFolder/test"
       file = dir <> "/jukebox.json"
 #endif
+-- #ifdef mingw32_HOST_OS
+--   let dir ="C:\\sharedFolder\\test" -- windows
+--       file = dir <> "\\jukebox.json"
+-- #else
+--   let dir ="/home/m/share/sharedFolder/test"
+--       file = dir <> "/jukebox.json"
+-- #endif
   tvar <- Engine.initLibrary dir file
   let logHandle =
         Handlers.Logger.Handle
