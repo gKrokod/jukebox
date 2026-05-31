bf51518bd67ca63485c2ccbbaedf0271adb40ee5
gKrokod <t9013054362@gmail.com>
2026-05-31 18:03:55 +0300

    repair

--- src/Hotkey/Ubunta.hs
+++ src/Hotkey/Ubunta.hs
@@ -3,66 +3,72 @@
 
 module Hotkey.Ubunta (getKey)
    where
-import Control.Concurrent.STM ( atomically, writeTVar, TVar )
+
+import Control.Concurrent.STM (atomically, writeTVar, TVar)
 import Control.Monad (forever, when)
-import Hotkey.Types ( Pause(..) )
+import Data.Bits ((.&.))
+import Hotkey.Types (Pause(..))
 import Graphics.X11.Xlib
-    ( anyModifier,
-      grabModeAsync,
-      keyPress,
-      keyPressMask,
-      xK_F7,
-      xK_F8,
-      xK_F9,
-      defaultScreen,
-      openDisplay,
-      rootWindow,
-      allocaXEvent,
-      get_EventType,
-      get_KeyEvent,
-      nextEvent,
-      selectInput,
-      sync,
-      grabKey,
-      keysymToKeycode )
+    ( anyModifier
+    , controlMask
+    , grabModeAsync
+    , keyPress
+    , keyPressMask
+    , xK_F7
+    , xK_F8
+    , xK_F9
+    , defaultScreen
+    , openDisplay
+    , rootWindow
+    , allocaXEvent
+    , get_EventType
+    , get_KeyEvent
+    , nextEvent
+    , selectInput
+    , sync
+    , grabKey
+    , keysymToKeycode
+    )
 
 getKey :: TVar Pause -> IO ()
 getKey pause = do
-    -- Подключаемся к X-серверу
     dpy <- openDisplay ""
 
-    let scr  = defaultScreen dpy
+    let scr = defaultScreen dpy
     root <- rootWindow dpy scr
 
-    -- Получаем keycode для F7 (должен совпасть с твоим 73)
-    -- f7Code <- keysymToKeycode dpy xK_A
     f7Code <- keysymToKeycode dpy xK_F7
     f8Code <- keysymToKeycode dpy xK_F8
     f9Code <- keysymToKeycode dpy xK_F9
-    putStrLn $ "F7 - pause \t F8 - resume \t F9 - next"
 
-    -- Глобально захватываем F7 на root-окне
-    -- AnyModifier, чтобы не ломалось из-за NumLock/CapsLock
+    putStrLn "Ctrl+F7 - pause\tCtrl+F8 - resume\tCtrl+F9 - next"
+
+    -- Глобально захватываем F7/F8/F9 на root-окне.
+    -- Используем anyModifier, а нужный Ctrl проверяем уже по state события.
+    -- Это помогает не ломаться из-за NumLock/CapsLock и прочих модификаторов. [web:186][web:196]
     grabKey dpy f7Code anyModifier root True grabModeAsync grabModeAsync
     grabKey dpy f8Code anyModifier root True grabModeAsync grabModeAsync
     grabKey dpy f9Code anyModifier root True grabModeAsync grabModeAsync
 
-    -- Просим у root события нажатия клавиш
     selectInput dpy root keyPressMask
     sync dpy False
 
     allocaXEvent $ \ev -> forever $ do
-    -- allocaXEvent $ \ev -> do
         nextEvent dpy ev
         t <- get_EventType ev
         when (t == keyPress) $ do
-            (_, _, _, _, _, _, _, _mods, keycode, _) <- get_KeyEvent ev
-            when (keycode == f7Code) $ do
-                atomically $ writeTVar pause On 
-                putStrLn "Нажата F7 (перехвачено глобально)"
-            when (keycode == f8Code) $ do
-                atomically $ writeTVar pause Off 
-                putStrLn "Нажата F8 (перехвачено глобально)"
-            when (keycode == f9Code) $ do
-                atomically $ writeTVar pause Next 
-                putStrLn "Нажата F9 (перехвачено глобально)"
+            (_, _, _, _, _, _, _, mods, keycode, _) <- get_KeyEvent ev
+
+            let ctrlPressed = (mods .&. controlMask) /= 0
+
+            when (ctrlPressed && keycode == f7Code) $ do
+                atomically $ writeTVar pause On
+                putStrLn "Нажата Ctrl+F7 (перехвачено глобально)"
+
+            when (ctrlPressed && keycode == f8Code) $ do
+                atomically $ writeTVar pause Off
+                putStrLn "Нажата Ctrl+F8 (перехвачено глобально)"
+
+            when (ctrlPressed && keycode == f9Code) $ do
+                atomically $ writeTVar pause Next
+                putStrLn "Нажата Ctrl+F9 (перехвачено глобально)"
--- src/Hotkey/Windows.hs
+++ src/Hotkey/Windows.hs
@@ -1,57 +1,72 @@
 {-# LANGUAGE CPP #-}
 module Hotkey.Windows (getKey)
    where
-import Control.Concurrent.STM ( atomically, writeTVar, TVar )
-import Control.Monad (forever, when)
-import Hotkey.Types ( Pause(..) )
+
+import Control.Concurrent.STM (atomically, writeTVar, TVar)
+import Hotkey.Types (Pause(..))
 import Control.Concurrent (threadDelay)
 import qualified Data.Bits as B
 import Graphics.Win32.Key
 
--- Какие клавиши нас интересуют
-data Key = KeyF7 | KeyF8 | KeyF9
-  deriving (Eq, Show)
-
--- Виртуальные коды F7/F8/F9 (VK_F7/VK_F8/VK_F9)
-vkF7, vkF8, vkF9 :: Int
-vkF7 = 0x76
-vkF8 = 0x77
-vkF9 = 0x78
+-- Виртуальные коды клавиш
+vkCtrl, vkF7, vkF8, vkF9 :: Int
+vkCtrl = 0x11   -- VK_CONTROL
+vkF7   = 0x76   -- VK_F7
+vkF8   = 0x77   -- VK_F8
+vkF9   = 0x78   -- VK_F9
 
 -- Проверка: клавиша сейчас нажата?
--- getAsyncKeyState возвращает 16-битное значение (WORD),
--- старший бит (0x8000) == клавиша зажата [web:43][web:84][web:91]
+-- GetAsyncKeyState: старший бит (0x8000) означает "клавиша сейчас зажата" [web:179][web:181]
 isDown :: Int -> IO Bool
 isDown vk = do
-#ifdef mingw32_HOST_OS
-  state <- getAsyncKeyState vk    -- :: WORD
-#else
-  let state = 0 :: Int            -- заглушка для не-Windows, если нужно компилировать
-#endif
+  state <- getAsyncKeyState vk
   let s :: Int
-      s = fromIntegral state      -- приводим к Int, чтобы .&. нормально типизировался
-  return ((s B..&. 0x8000) /= 0)
+      s = fromIntegral state
+  pure ((s B..&. 0x8000) /= 0)
 
--- Ждём отпускания конкретной клавиши, чтобы не ловить автоповтор
-waitRelease :: Int -> IO ()
-waitRelease vk = do
-  d <- isDown vk
-  if d
-    then threadDelay 50000 >> waitRelease vk
-    else return ()
+-- Нажаты ли одновременно Ctrl + нужная клавиша
+isCtrlComboDown :: Int -> IO Bool
+isCtrlComboDown vk = do
+  ctrl <- isDown vkCtrl
+  key  <- isDown vk
+  pure (ctrl && key)
 
--- Блокирующая функция: ждём, пока пользователь нажмёт F7/F8/F9
-getKey :: IO Key
-getKey = loop
+-- Ждём, пока будут отпущены и Ctrl, и сама клавиша
+waitReleaseCombo :: Int -> IO ()
+waitReleaseCombo vk = do
+  ctrl <- isDown vkCtrl
+  key  <- isDown vk
+  if ctrl || key
+    then threadDelay 50000 >> waitReleaseCombo vk
+    else pure ()
+
+-- Глобальная обработка:
+-- Ctrl+F7 -> On
+-- Ctrl+F8 -> Off
+-- Ctrl+F9 -> Next
+getKey :: TVar Pause -> IO ()
+getKey pauseVar = loop
   where
     loop = do
-      d7 <- isDown vkF7
-      d8 <- isDown vkF8
-      d9 <- isDown vkF9
-      case () of
-        _ | d7 -> waitRelease vkF7 >> return KeyF7
-          | d8 -> waitRelease vkF8 >> return KeyF8
-          | d9 -> waitRelease vkF9 >> return KeyF9
-          | otherwise -> do
+      d7 <- isCtrlComboDown vkF7
+      d8 <- isCtrlComboDown vkF8
+      d9 <- isCtrlComboDown vkF9
+
+      if d7
+        then do
+          atomically $ writeTVar pauseVar On
+          waitReleaseCombo vkF7
+          loop
+        else if d8
+          then do
+            atomically $ writeTVar pauseVar Off
+            waitReleaseCombo vkF8
+            loop
+          else if d9
+            then do
+              atomically $ writeTVar pauseVar Next
+              waitReleaseCombo vkF9
+              loop
+            else do
               threadDelay 50000
               loop
